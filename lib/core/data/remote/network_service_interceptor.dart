6b7ee6aad1698aef26f2157518a790213b0f27c6
CJ-404 <cj.ptsz@yahoo.com>
2024-12-21 20:33:51 +0530

    improve: create token service to handle tokens by extracting this responsibility from network service interceptor

--- lib/core/data/remote/network_service_interceptor.dart
+++ lib/core/data/remote/network_service_interceptor.dart
@@ -1,26 +1,25 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gll/common/http_status/status_code.dart';
-import 'package:gll/core/data/local/secure_storage/Isecure_storage.dart';
-import 'package:gll/core/data/local/secure_storage/secure_storage_const.dart';
-import '../../../common/dtos/refresh_token_response.dart';
-import '../local/secure_storage/secure_storage.dart';
+import 'package:gll/core/data/remote/token/itoken_service.dart';
+import 'package:gll/core/data/remote/token/token_service.dart';
 
 final networkServiceInterceptorProvider = Provider.family<NetworkServiceInterceptor, Dio>((ref, dio) {
-  final secureStorage = ref.watch(secureStorageProvider);
-  return NetworkServiceInterceptor(secureStorage, dio);
+  final tokenService = ref.watch(tokenServiceProvider(dio));
+
+  return NetworkServiceInterceptor(tokenService, dio);
 });
 
 final class NetworkServiceInterceptor extends Interceptor {
 
-  final ISecureStorage _secureStorage;
+  final ITokenService _tokenService;
   final Dio _dio;
 
-  NetworkServiceInterceptor(this._secureStorage, this._dio);
+  NetworkServiceInterceptor(this._tokenService, this._dio);
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
-    final accessToken = await _secureStorage.read(key: accessTokenKey);
+    final accessToken = await _tokenService.getAccessToken();
 
     options.headers['Content-type'] = 'application/json';
     options.headers['Accept'] = 'application/json';
@@ -33,37 +32,28 @@ final class NetworkServiceInterceptor extends Interceptor {
   void onError(DioException err, ErrorInterceptorHandler handler) async {
     // handle unauthorized error
     if (err.response?.statusCode == unauthorized) {
-      final token = await _secureStorage.read(key: refreshTokenKey);
+      final token = await _tokenService.getRefreshToken();
 
       try {
-        final response = await _refreshToken(_dio, token);
+        final result = await _tokenService.refreshToken(token);
 
-        final json = response.data;
-        final result = RefreshTokenResponse.fromJson(json?? {});
+        final accessToken = result.data.accessToken;
+        final refreshToken = result.data.refreshToken;
 
-        final statusCode = response.statusCode;
-        if (statusCode == success) {
-          final accessToken = result.data.accessToken;
-          final refreshToken = result.data.refreshToken;
-          
-          // save the new access token and refresh token
-          await _setAuthData(accessToken, refreshToken);
+        // save the new access token and refresh token
+        await _tokenService.saveTokens(accessToken, refreshToken);
 
-          final options = err.requestOptions;
-          // update the request headers with new access token
-          options.headers['Authorization'] = 'Bearer $accessToken';
+        final options = err.requestOptions;
+        // update the request headers with new access token
+        options.headers['Authorization'] = 'Bearer $accessToken';
 
-          // repeat the request with the updated access token
-          return handler.resolve(await _dio.fetch(options));
-        }
+        // repeat the request with the updated access token
+        return handler.resolve(await _dio.fetch(options));
       }
       on DioException catch (e) {
         if  (e.response?.statusCode == refreshTokenExpired) {
           // delete the access token and refresh token from secure storage
-          await _clearAuthData();
-
-          // set the status code to 498 to indicate that the token is invalid
-          err.response?.statusCode = refreshTokenExpired;
+          await _tokenService.clearTokens();
 
           return handler.next(err);
         }
@@ -72,27 +62,7 @@ final class NetworkServiceInterceptor extends Interceptor {
         return handler.next(err);
       }
     }
-
     // continue with the error
     return handler.next(err);
-  }
-
-  Future<void> _clearAuthData() async {
-    await _secureStorage.delete(key: accessTokenKey);
-    await _secureStorage.delete(key: refreshTokenKey);
-  }
-
-  Future<void> _setAuthData(String accessToken, String refreshToken) async {
-    await _secureStorage.write(key: accessTokenKey, value: accessToken);
-    await _secureStorage.write(key: refreshTokenKey, value: refreshToken);
-  }
-
-  Future<Response<Map<String, dynamic>>> _refreshToken(Dio dio, String? token) async {
-    return await _dio.post<Map<String, dynamic>>(
-      '/refresh_token',
-      data: {
-        'refreshToken': token,
-      },
-    );
   }
 }
---
+++ lib/core/data/remote/token/token_service.dart
@@ -0,0 +1,63 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gll/core/data/local/secure_storage/Isecure_storage.dart';
+import 'package:gll/core/data/remote/token/itoken_service.dart';
+import '../../../../common/dtos/refresh_token_response.dart';
+import '../../../../common/http_status/status_code.dart';
+import '../../local/secure_storage/secure_storage.dart';
+import '../../local/secure_storage/secure_storage_const.dart';
+
+final tokenServiceProvider = Provider.family<ITokenService, Dio>((ref, dio) {
+  final secureStorage = ref.watch(secureStorageProvider);
+
+  return TokenService(dio, secureStorage);
+});
+
+class TokenService implements ITokenService {
+  final Dio _dio;
+  final ISecureStorage _secureStorage;
+
+  TokenService(this._dio, this._secureStorage);
+
+  @override
+  Future<void> clearTokens() {
+    return Future.wait([
+      _secureStorage.delete(key: accessTokenKey),
+      _secureStorage.delete(key: refreshTokenKey),
+    ]);
+  }
+
+  @override
+  Future<String?> getAccessToken() => _secureStorage.read(key: accessTokenKey);
+
+  @override
+  Future<String?> getRefreshToken() => _secureStorage.read(key: refreshTokenKey);
+
+  @override
+  Future<RefreshTokenResponse> refreshToken(String? refreshToken) async {
+    final response =  await _dio.post<Map<String, dynamic>>(
+      '/refresh_token',
+      data: {
+        'refreshToken': refreshToken,
+      },
+    );
+
+    if (response.statusCode == success) {
+      return RefreshTokenResponse.fromJson(response.data?? {});
+    }
+    else {
+      throw DioException(
+        requestOptions: response.requestOptions,
+        response: response,
+      );
+    }
+  }
+
+  @override
+  Future<void> saveTokens(String accessToken, String refreshToken) {
+    return Future.wait([
+      _secureStorage.write(key: accessTokenKey, value: accessToken),
+      _secureStorage.write(key: refreshTokenKey, value: refreshToken),
+    ]);
+  }
+}
