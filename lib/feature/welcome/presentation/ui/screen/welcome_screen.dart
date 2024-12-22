5d7d4379012e72532b060ede06c9a9cbbcf00bd3
CJ-404 <cj.ptsz@yahoo.com>
2024-12-22 10:37:22 +0530

    feat: attch login & signup overlay with animations into the welcome screen

--- lib/feature/welcome/presentation/ui/screen/welcome_screen.dart
+++ lib/feature/welcome/presentation/ui/screen/welcome_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gll/feature/welcome/presentation/ui/widget/get_started.dart';
 import 'package:gll/feature/welcome/presentation/ui/widget/logo.dart';
 
+import '../provider/animation_visibility_provider.dart';
+
 class WelcomeScreen extends ConsumerStatefulWidget {
   const WelcomeScreen({super.key});
 
@@ -14,17 +16,32 @@ class _WelcomeScreenState extends ConsumerState<WelcomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    final animationVisibility = ref.watch(animationVisibilityProvider);
+
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          // Top 1/3: Logo and description
-          Logo(),
-          // Bottom 2/3: Stack area
-          Expanded(
-            child: GetStarted(),
+      body: AnimatedOpacity(
+        duration: const Duration(milliseconds: 500),
+        opacity: animationVisibility ? 1 : 0,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          transform: Matrix4.translationValues(
+            0,
+            animationVisibility ? 0 : 100,
+            0,
           ),
-        ],
+          child: Column(
+            children: [
+              // Top 1/3: Logo and description
+              Logo(),
+              // Bottom 2/3: Stack area
+              Expanded(
+                child: GetStarted(),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
