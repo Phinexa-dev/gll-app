e3361829e8d74827de6c2c0aee90a895abfede28
Pabasara715 <pabasaravihanga715@gmail.com>
2025-01-13 04:09:41 +0530

    Update routes, colors and fonts

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+   GMSServices.provideAPIKey("AIzaSyC7yl3IA5fKomkouQ5H8k7f6Y3pms6L-LA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
