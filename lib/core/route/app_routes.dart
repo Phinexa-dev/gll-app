f4c8f7603a7b37eb16a36ba1d286df5b00cab17c
Pabasara715 <pabasaravihanga715@gmail.com>
2025-02-17 00:08:44 +0530

    Add new routes

--- lib/core/route/app_routes.dart
+++ lib/core/route/app_routes.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:gll/common/widget/event_view_screen_widget.dart';
 import 'package:gll/core/route/route_name.dart';
 import 'package:gll/feature/bottom_bar/presentation/ui/screen/bottom_bar.dart';
-import 'package:gll/feature/home/presentation/ui/screen/pre_survay_trainer/background_information_screen.dart';
-import 'package:gll/feature/home/presentation/ui/screen/pre_survay_trainer/interests_and_engagement_screen.dart';
-import 'package:gll/feature/home/presentation/ui/screen/registration_form.dart';
+import 'package:gll/feature/events/presentation/ui/screen/post_surveys/post_LA_survey/application_of_skills_screen.dart';
+import 'package:gll/feature/events/presentation/ui/screen/post_surveys/post_LA_survey/module_specific_feedback_screen.dart';
+import 'package:gll/feature/events/presentation/ui/screen/post_surveys/post_LA_survey/trainer_facilitation_feedback_screen.dart';
+import 'package:gll/feature/events/presentation/ui/screen/registration_form.dart';
 import 'package:gll/feature/other/presentation/ui/screen/camp_detail_screen.dart';
 import 'package:gll/feature/other/presentation/ui/screen/profile_screen.dart';
 import 'package:gll/feature/other/presentation/ui/screen/sip_map.dart';
@@ -14,10 +15,17 @@ import 'package:gll/feature/resources/presentation/ui/widgets/sip_report_view_mo
 import 'package:go_router/go_router.dart';
 
 import '../../feature/events/presentation/ui/screen/events_screen.dart';
-import '../../feature/home/presentation/ui/screen/pre_survay/background_information_screen.dart';
-import '../../feature/home/presentation/ui/screen/pre_survay/goals_expectations_screen.dart';
-import '../../feature/home/presentation/ui/screen/pre_survay/interests_and_engagement_screen.dart';
-import '../../feature/home/presentation/ui/screen/pre_survay_trainer/goals_expectations_screen.dart';
+import '../../feature/events/presentation/ui/screen/post_surveys/post_LA_survey/overall_program_feedback_screen.dart';
+import '../../feature/events/presentation/ui/screen/post_surveys/post_TTT_survey/application_of_skills_screen.dart';
+import '../../feature/events/presentation/ui/screen/post_surveys/post_TTT_survey/module_specific_feedback_screen.dart';
+import '../../feature/events/presentation/ui/screen/post_surveys/post_TTT_survey/overall_program_feedback_screen.dart';
+import '../../feature/events/presentation/ui/screen/post_surveys/post_TTT_survey/trainer_facilitation_feedback_screen.dart';
+import '../../feature/events/presentation/ui/screen/pre_surveys/pre_survay_LA/background_information_screen.dart';
+import '../../feature/events/presentation/ui/screen/pre_surveys/pre_survay_LA/goals_expectations_screen.dart';
+import '../../feature/events/presentation/ui/screen/pre_surveys/pre_survay_LA/interests_and_engagement_screen.dart';
+import '../../feature/events/presentation/ui/screen/pre_surveys/pre_survay_TTT/background_information_screen.dart';
+import '../../feature/events/presentation/ui/screen/pre_surveys/pre_survay_TTT/goals_expectations_screen.dart';
+import '../../feature/events/presentation/ui/screen/pre_surveys/pre_survay_TTT/interests_and_engagement_screen.dart';
 import '../../feature/other/presentation/ui/screen/edit_profile_screen.dart';
 import '../../feature/other/presentation/ui/widget/map_view_widget.dart';
 import '../../feature/resources/presentation/ui/screen/resources_tab_screen.dart';
@@ -75,6 +83,8 @@ final appRoutes = [
     parentNavigatorKey: navigationKey,
     builder: (context, state) => RegistrationForm(),
   ),
+
+  ///LA PreSurvey Routes
   GoRoute(
     path: RouteName.preSurvey,
     name: RouteName.preSurvey,
@@ -93,6 +103,8 @@ final appRoutes = [
     parentNavigatorKey: navigationKey,
     builder: (context, state) => InterestsAndEngagementScreen(),
   ),
+
+  ///TT PreSurvey Routes
   GoRoute(
     path: RouteName.ttPreSurvey,
     name: RouteName.ttPreSurvey,
@@ -110,6 +122,58 @@ final appRoutes = [
     name: RouteName.ttInterestsAndEngagementScreen,
     parentNavigatorKey: navigationKey,
     builder: (context, state) => TTInterestsAndEngagementScreen(),
+  ),
+
+  ///LA PostSurvey Routes
+  GoRoute(
+    path: RouteName.laOverallProgramFeedbackScreen,
+    name: RouteName.laOverallProgramFeedbackScreen,
+    parentNavigatorKey: navigationKey,
+    builder: (context, state) => LAOverallProgramFeedbackScreen(),
+  ),
+  GoRoute(
+    path: RouteName.laModuleSpecificFeedbackScreen,
+    name: RouteName.laModuleSpecificFeedbackScreen,
+    parentNavigatorKey: navigationKey,
+    builder: (context, state) => LAModuleSpecificFeedbackScreen(),
+  ),
+  GoRoute(
+    path: RouteName.laTrainerFacilitationFeedbackScreen,
+    name: RouteName.laTrainerFacilitationFeedbackScreen,
+    parentNavigatorKey: navigationKey,
+    builder: (context, state) => LATrainerFacilitationFeedbackScreen(),
+  ),
+  GoRoute(
+    path: RouteName.laApplicationOfSkillsScreen,
+    name: RouteName.laApplicationOfSkillsScreen,
+    parentNavigatorKey: navigationKey,
+    builder: (context, state) => LAApplicationOfSkillsScreen(),
+  ),
+
+  ///TTT PostSurvey Routes
+  GoRoute(
+    path: RouteName.tttOverallProgramFeedbackScreen,
+    name: RouteName.tttOverallProgramFeedbackScreen,
+    parentNavigatorKey: navigationKey,
+    builder: (context, state) => TTTOverallProgramFeedbackScreen(),
+  ),
+  GoRoute(
+    path: RouteName.tttModuleSpecificFeedbackScreen,
+    name: RouteName.tttModuleSpecificFeedbackScreen,
+    parentNavigatorKey: navigationKey,
+    builder: (context, state) => TTTModuleSpecificFeedbackScreen(),
+  ),
+  GoRoute(
+    path: RouteName.tttTrainerFacilitationFeedbackScreen,
+    name: RouteName.tttTrainerFacilitationFeedbackScreen,
+    parentNavigatorKey: navigationKey,
+    builder: (context, state) => TTTTrainerFacilitationFeedbackScreen(),
+  ),
+  GoRoute(
+    path: RouteName.tttApplicationOfSkillsScreen,
+    name: RouteName.tttApplicationOfSkillsScreen,
+    parentNavigatorKey: navigationKey,
+    builder: (context, state) => TTTApplicationOfSkillsScreen(),
   ),
   GoRoute(
     path: RouteName.resourceMore,
