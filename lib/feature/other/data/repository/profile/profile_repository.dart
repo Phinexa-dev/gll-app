1656bce50bfaa257c08665d44e04e7566147d5b9
CJ-404 <cj.ptsz@yahoo.com>
2025-03-02 15:32:27 +0530

    create: repositories for other feature to use api calls and dtos

---
+++ lib/feature/other/data/repository/profile/profile_repository.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gll/feature/other/data/dto/request/profile/editSocials/edit_social_request.dart';
+import 'package:gll/feature/other/data/dto/request/profile/edit_profile/edit_profile_request.dart';
+import 'package:gll/feature/other/data/dto/response/profile/edit_profile_response.dart';
+import 'package:gll/feature/other/data/source/remote/profile/edit_profile_api.dart';
+import 'iprofile_repository.dart';
+
+final profileRepositoryProvider = Provider<IProfileRepository>((ref) {
+  final EditProfileApi profileApi = ref.watch(editProfileApiProvider);
+  return ProfileRepository(profileApi);
+});
+
+final class ProfileRepository implements IProfileRepository {
+  final EditProfileApi _profileApi;
+
+  ProfileRepository(this._profileApi);
+
+  @override
+  Future<EditProfileResponse> editProfile(EditProfileRequest data, int userId) async {
+    try {
+
+      final response = await _profileApi.editProfile(data, userId);
+      return response;
+
+    } on DioException catch (_) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<EditProfileResponse> editSkills(EditSocialRequest data, int userId) async {
+    try {
+
+      final response = await _profileApi.editSkills(data, userId);
+      return response;
+
+    } on DioException catch (_) {
+      rethrow;
+    }
+  }
+}
---
+++ lib/feature/other/data/repository/skill/skill_repository.dart
@@ -0,0 +1,53 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gll/feature/other/data/dto/request/skill/add_skill_request.dart';
+import 'package:gll/feature/other/data/dto/response/skill/skill_response.dart';
+import 'package:gll/feature/other/data/source/remote/skill/manage_skills_api.dart';
+import 'iskill_repository.dart';
+
+
+final skillRepositoryProvider = Provider<ISkillRepository>((ref) {
+  final ManageSkillsApi skillApi = ref.watch(manageSkillsApiProvider);
+  return SkillRepository(skillApi);
+});
+
+final class SkillRepository implements ISkillRepository {
+  final ManageSkillsApi _skillApi;
+
+  SkillRepository(this._skillApi);
+
+  @override
+  Future<void> addSkill(AddSkillRequest data) async {
+    try {
+
+      await _skillApi.addSkill(data);
+
+    } on DioException catch (_) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<SkillResponse> deleteSkill(int skillId) async {
+    try {
+
+      final response = await _skillApi.deleteSkill(skillId);
+      return response;
+
+    } on DioException catch (_) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<SkillResponse>> getSkills() async {
+    try {
+
+      final response = await _skillApi.getSkills();
+      return response;
+
+    } on DioException catch (_) {
+      rethrow;
+    }
+  }
+}
