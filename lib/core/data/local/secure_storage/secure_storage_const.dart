18bc03074ae44bd576ce98307955d52780947f93
CJ-404 <cj.ptsz@yahoo.com>
2025-03-02 08:05:23 +0530

    create: core data service o save & retrieve user data

--- lib/core/data/local/secure_storage/secure_storage_const.dart
+++ lib/core/data/local/secure_storage/secure_storage_const.dart
@@ -1,2 +1,5 @@
 const String accessTokenKey = 'access_token';
 const String refreshTokenKey = 'refresh_token';
+const String userId = 'user_id';
+const String userName = 'user_name';
+const String userEmail = 'user_email';
