377cace9d5062e5d2669056b7b060bf0067c8091
Pabasara715 <pabasaravihanga715@gmail.com>
2025-05-10 18:53:10 +0530

    Add country prop to user model

--- lib/core/data/local/user/model/user_model.dart
+++ lib/core/data/local/user/model/user_model.dart
@@ -6,13 +6,12 @@ part 'user_model.g.dart';
 @freezed
 class UserModel with _$UserModel {
   const factory UserModel({
-    @JsonKey(name: "id")
-    required int id,
-    @JsonKey(name: "fullName")
-    required String fullName,
-    @JsonKey(name: "email")
-    required String email,
+    @JsonKey(name: "id") required int id,
+    @JsonKey(name: "fullName") required String fullName,
+    @JsonKey(name: "email") required String email,
+    @JsonKey(name: "country") required String country,
   }) = _UserModel;
 
-  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 }
