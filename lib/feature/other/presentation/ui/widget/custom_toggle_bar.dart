2570a4c52476bd1080e21238863e79da6e67cdc3
Pabasara715 <pabasaravihanga715@gmail.com>
2025-05-18 12:43:32 +0530

    Fix toggle button layout

--- lib/feature/other/presentation/ui/widget/custom_toggle_bar.dart
+++ lib/feature/other/presentation/ui/widget/custom_toggle_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gll/common/theme/fonts.dart';
+
 import '../provider/toggle_button_provider.dart';
 
 class CustomToggleBar extends ConsumerWidget {
@@ -23,39 +24,68 @@ class CustomToggleBar extends ConsumerWidget {
         renderBorder: false,
         borderRadius: BorderRadius.circular(100),
         fillColor: Colors.white,
-        color: Colors.grey, // Default text color
-        selectedColor: Colors.black, // Text color when selected
+        color: Colors.grey,
+        // Default text color
+        selectedColor: Colors.black,
+        // Text color when selected
         borderWidth: 1,
         constraints: const BoxConstraints(
           minHeight: 45,
           minWidth: 138,
         ),
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: toggleButtonState[0]? Color(0xFF3993A1).withAlpha(60) : Colors.white,
-              border: toggleButtonState[0]? Border.all(color: Color(0xFF3993A1), width: 1) : null,
-              borderRadius: BorderRadius.all(Radius.circular(100)),
-            ),
-            constraints: const BoxConstraints(
-              minWidth: 120,
-              minHeight:40,
-            ),
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: Text('Personal', style: PhinexaFont.contentRegular.copyWith(color: toggleButtonState[0]? Colors.black : Colors.grey, fontWeight: toggleButtonState[0]? FontWeight.bold : FontWeight.normal)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2.0, vertical: 8),
+            child: Container(
+                decoration: BoxDecoration(
+                  color: toggleButtonState[0]
+                      ? Color(0xFF3993A1).withAlpha(60)
+                      : Colors.white,
+                  border: toggleButtonState[0]
+                      ? Border.all(color: Color(0xFF3993A1), width: 1)
+                      : null,
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                ),
+                constraints: const BoxConstraints(
+                  minWidth: 120,
+                  minHeight: 40,
+                ),
+                child: Center(
+                  child: Text('Personal',
+                      style: PhinexaFont.contentRegular.copyWith(
+                          color:
+                              toggleButtonState[0] ? Colors.black : Colors.grey,
+                          fontWeight: toggleButtonState[0]
+                              ? FontWeight.bold
+                              : FontWeight.normal)),
+                )),
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: toggleButtonState[1]? Color(0xFF3993A1).withAlpha(60) : Colors.white,
-              border: toggleButtonState[1]? Border.all(color: Color(0xFF3993A1), width: 1) : null,
-              borderRadius: BorderRadius.all(Radius.circular(100)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2.0, vertical: 8),
+            child: Container(
+              decoration: BoxDecoration(
+                color: toggleButtonState[1]
+                    ? Color(0xFF3993A1).withAlpha(60)
+                    : Colors.white,
+                border: toggleButtonState[1]
+                    ? Border.all(color: Color(0xFF3993A1), width: 1)
+                    : null,
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+              ),
+              constraints: const BoxConstraints(
+                minWidth: 120,
+                minHeight: 40,
+              ),
+              child: Center(
+                child: Text('Educational',
+                    style: PhinexaFont.contentRegular.copyWith(
+                        color:
+                            toggleButtonState[1] ? Colors.black : Colors.grey,
+                        fontWeight: toggleButtonState[1]
+                            ? FontWeight.bold
+                            : FontWeight.normal)),
+              ),
             ),
-            constraints: const BoxConstraints(
-              minWidth: 120,
-              minHeight:40,
-            ),
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: Text('Educational', style: PhinexaFont.contentRegular.copyWith(color: toggleButtonState[1]? Colors.black : Colors.grey, fontWeight: toggleButtonState[1]? FontWeight.bold : FontWeight.normal)),
           ),
         ],
       ),
