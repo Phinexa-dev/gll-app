f4eb88299ce4dafd11ff81a25e01aa92725d3748
Pabasara715 <pabasaravihanga715@gmail.com>
2025-08-22 02:28:15 +0530

    Remove data fields from post surveys

--- lib/feature/events/presentation/ui/screen/post_surveys/post_LA_survey/overall_program_feedback_screen.dart
+++ lib/feature/events/presentation/ui/screen/post_surveys/post_LA_survey/overall_program_feedback_screen.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gll/common/theme/fonts.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 
 import '../../../../../../../../common/widget/custom_button.dart';
 import '../../../../../../../../core/route/route_name.dart';
 import '../../../../../../../common/theme/colors.dart';
 import '../../../provider/combine_response.dart';
 import '../../../provider/survey_radio_string_response_provider.dart';
-import '../../../provider/text_and_dropdown_reponses_provider.dart';
 import '../../../widgets/survey_question_widget.dart';
 
 class LAOverallProgramFeedbackScreen extends ConsumerStatefulWidget {
@@ -27,8 +25,6 @@ class LAOverallProgramFeedbackScreen extends ConsumerStatefulWidget {
 
 class _LAOverallProgramFeedbackScreenState
     extends ConsumerState<LAOverallProgramFeedbackScreen> {
-  DateTime? _selectedDate;
-  final _dateError = ValueNotifier<String?>(null);
 
   final _questionErrors = {
     "The Leadership Academy met my expectations": ValueNotifier<String?>(null),
@@ -46,7 +42,6 @@ class _LAOverallProgramFeedbackScreenState
 
   @override
   void dispose() {
-    _dateError.dispose();
     for (var error in _questionErrors.values) {
       error.dispose();
     }
@@ -91,14 +86,6 @@ class _LAOverallProgramFeedbackScreenState
     bool isValid = true;
     String errorMessage = "The following fields are required:\n";
 
-    if (_selectedDate == null) {
-      _dateError.value = 'Please select a date';
-      isValid = false;
-      errorMessage += "- Date\n";
-    } else {
-      _dateError.value = null;
-    }
-
     final responses = ref.read(radioStringQuestionResponseProvider);
     for (var question in _questionErrors.keys) {
       if (responses[question] == null) {
@@ -120,24 +107,6 @@ class _LAOverallProgramFeedbackScreenState
     }
   }
 
-  Future<void> _selectDate(BuildContext context) async {
-    final picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-
-    if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-        _dateError.value = null;
-      });
-      ref
-          .read(surveyTextFieldResponseProvider.notifier)
-          .updateResponse("Date", _selectedDate.toString());
-    }
-  }
 
   Widget _buildQuestionWidget(
     String question,
@@ -189,10 +158,7 @@ class _LAOverallProgramFeedbackScreenState
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(height: 20),
-                ValueListenableBuilder<String?>(
-                  valueListenable: _dateError,
-                  builder: (context, error, child) {
-                    return Column(
+                 Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         RichText(
@@ -201,34 +167,7 @@ class _LAOverallProgramFeedbackScreenState
                             children: [
                               TextSpan(
                                 text:
-                                    "Thank you for participating in the Leadership Academy on ",
-                              ),
-                              WidgetSpan(
-                                child: InkWell(
-                                  onTap: () => _selectDate(context),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text(
-                                        _selectedDate == null
-                                            ? " \t ______________ "
-                                            : '\t ${DateFormat.yMMMMd().format(_selectedDate!)}',
-                                        style: PhinexaFont.highlightRegular
-                                            .copyWith(
-                                              decoration: _selectedDate != null
-                                                  ? TextDecoration.underline
-                                                  : TextDecoration.none,
-                                              fontWeight: FontWeight.w300,
-                                            ),
-                                      ),
-                                      const SizedBox(width: 8),
-                                      Icon(
-                                        Icons.calendar_today_outlined,
-                                        size: 14,
-                                      ),
-                                    ],
-                                  ),
-                                ),
+                                    "Thank you for participating in the Leadership Academy",
                               ),
                               TextSpan(
                                 text:
@@ -238,18 +177,9 @@ class _LAOverallProgramFeedbackScreenState
                             ],
                           ),
                         ),
-                        if (error != null)
-                          Padding(
-                            padding: const EdgeInsets.only(top: 4),
-                            child: Text(
-                              error,
-                              style: TextStyle(color: Colors.red),
-                            ),
-                          ),
                       ],
-                    );
-                  },
-                ),
+                    ),
+
                 SizedBox(height: 20),
                 Text(
                   "Overall Program Feedback",
